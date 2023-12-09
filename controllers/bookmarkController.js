e9e826b38fe1836c20c95ea343cdaeb91d1c4380
keygenext <jonathantheresantojob@gmail.com>
2023-12-09 19:23:27 +0700

    update bookmark controller

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -37,8 +37,9 @@ module.exports = {
     getBookmarks: async (req, res) => {
         try {
              const bookmarks = await Bookmark.find({agentId: req.params.agentId});
+             const { password, __v, createdAt, ...others} = bookmarks._doc;
              const userId = req.params.agentId;
-             res.status(200).json(bookmarks, userId);
+             res.status(200).json({...others, userId});
          } catch ( error ) {
              res.status(500).json(error);
          }
