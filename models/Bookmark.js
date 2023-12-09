154bb62e3fff769f0f0a2d5ee2ea96c1c0e131f7
keygenext <jonathantheresantojob@gmail.com>
2023-12-09 19:42:25 +0700

    update

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,20 +1,19 @@
 const mongoose = require("mongoose");
 
 const BookmarkSchema = new mongoose.Schema(
-    {
-        job: {
-            id: {type: String, require: true},
-            title: {type: String, require: true},
-            location: {type: String, require: true},
-            company: {type: String, require: true},
-            salary: {type: String, require: true},
-            period: {type: String, require: true},
-            contract: {type: String, require: true},
-            imageUrl: {type: String, require: true},
-            agentId: {type: String, require: true},
-        },
-        userId: {type: String, require: true},
-    }, {timestamps: true}
+  {
+    id: { type: String, require: true },
+    title: { type: String, require: true },
+    location: { type: String, require: true },
+    company: { type: String, require: true },
+    salary: { type: String, require: true },
+    period: { type: String, require: true },
+    contract: { type: String, require: true },
+    imageUrl: { type: String, require: true },
+    agentId: { type: String, require: true },
+    userId: { type: String, require: true },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Bookmark", BookmarkSchema);
