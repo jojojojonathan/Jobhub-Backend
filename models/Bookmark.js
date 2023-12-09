3fe3600f2aff8fe4df2bada3416e19cff29d1fd4
keygenext <jonathantheresantojob@gmail.com>
2023-12-09 19:11:54 +0700

    update bookmark model

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 const BookmarkSchema = new mongoose.Schema(
     {
         job: {type: String, require: true},
-        userId: {type: String, require: true},
         title: {type: String, require: true},
-        imageUrl: {type: String, require: true},
-        company: {type: String, require: true},
         location: {type: String, require: true},
+        company: {type: String, require: true},
+        salary: {type: String, require: true},
+        period: {type: String, require: true},
+        contract: {type: String, require: true},
+        imageUrl: {type: String, require: true},
+        agentId: {type: String, require: true},
     }, {timestamps: true}
 );
 
